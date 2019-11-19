18baa98d28fb49700cf1f40cecef92328cd08a1d
Damian Wędzikowski <dwedzikowski@gmail.com>
2019-11-19 17:01:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -153,23 +153,12 @@ def main():
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
-
-    # test = filters.prewitt(gray)
     denoised = restoration.denoise_nl_means(gray, h=0.95)
-    # blurred = cv2.GaussianBlur(denoised, (5, 5), 0)
-    #thres = cv2.threshold(denoised, 60, 255, cv2.THRESH_BINARY)[1]
     threshold = filters.threshold_minimum(denoised)
     thres = denoised > threshold
 
-
-    # ihc_hed = rgb2hed(image)
-
-    # h = exposure.rescale_intensity(ihc_hed[:, :, 0], out_range=(0, 1))
-    # d = exposure.rescale_intensity(ihc_hed[:, :, 2], out_range=(0, 1))
-    # zdh = np.dstack((np.zeros_like(h), d, h))
     canny = feature.canny(thres, sigma=3)
     dilated = mp.binary_dilation(canny, selem=selem)
-    print(selem)
     edges = to_uint8(dilated)
 
     contours, hierarchy = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -177,102 +166,21 @@ def main():
     # Loop over the contours
     counter = 0
     for c, h in zip(contours, hierarchy[0]):
-        # Compute moments of the contours
-        M = cv2.moments(c)
-        cX = 0
-        cY = 0
         c = c.astype("float")
         c *= ratio
         c = c.astype("int")
         cv2.drawContours(image, [c], -1, (0, 255, 0), 5)
         if h[0] == -1 and h[1] == -1 and h[2] == -1:
             counter += 1
-        #
-        #     # Compute the centroids of the dies' outer contours
-        #     cX = int((M["m10"] / M["m00"]) * ratio)
-        #     cY = int((M["m01"] / M["m00"]) * ratio)
-        #     # Move the Y coordinate to the bottom of a die
-        #     cY += int(ratio*(np.sqrt(M["m00"])*np.sqrt(2))/2)
-        #     cv2.putText(image, "Kostka", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-        #                 0.5, (255, 255, 255), 2)
+
     cv2.putText(image, "Total: "+str(counter), (int(len(image[0])/4), int(0.95*len(image))),
                 cv2.FONT_HERSHEY_SIMPLEX, 10/(4000/len(image)), (255, 255, 255), 5)
-    # cv2.drawContours(resized, cnt, -1, (0, 255, 0), 1)
-    # image = imutils.resize(resized, width=800)
-    # cnt = 0
-    # for i in hierarchy[0]:
-    #     if i[0] == -1 and i[1] == -1 and i[2] == -1:
-    #         cnt += 1
-    #
-    # print(cnt)
+
     print_hierarchy(hierarchy[0], contours, ratio, image)
 
     img = Image.fromarray(image)
     img.save(name+'_proc.jpg')
     img.show()
-
-    # height, width = int(500 / image.shape[1] * image.shape[0]), 500
-    # image_resize = transform.resize(image, (height, width), anti_aliasing=True)
-
-    # image = img_as_float(image_resize)
-
-    # Immunohistochemical staining colors separation
-    # ihc_hed = rgb2hed(image)
-
-    # h = exposure.rescale_intensity(ihc_hed[:, :, 0], out_range=(0, 1))
-    # d = exposure.rescale_intensity(ihc_hed[:, :, 2], out_range=(0, 1))
-    # zdh = np.dstack((np.zeros_like(h), d, h))
-
-    # image = rgb2gray(zdh)
-    # image = Image.fromarray((image * 255).astype(np.uint8))
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # edges = rgb2gray(image)
-
-    # Adding this leaves only the dice outline somehow
-    # edges = restoration.denoise_nl_means(edges, h=0.95)
-
-    # Option 1
-    # edges = feature.canny(gray, sigma=5)
-    # edges = mp.binary_dilation(edges, selem=selem)
-    # edges = imutils.grab_contours(edges)
-
-    # Option 2
-    # edges = feature.canny(edges, sigma=5)
-    # edges = filters.prewitt(edges)
-    # edges = mp.binary.binary_closing(edges)
-
-    # image = restoration.denoise_nl_means(image, h=0.95)
-    # threshold = filters.threshold_minimum(image)
-    # image = image > threshold
-
-    # image = filters.prewitt(image)
-
-    # labeled, objects = scipy.ndimage.label(edges)
-    #
-    # region = regionprops(labeled)
-    #
-    # for j in range(len(edges)):
-    #     for k in range(len(edges[j])):
-    #         if edges[j][k]:  # if appropriate label is present
-    #             image[j][k] = [0, 0, 1]
-    #
-    # for cent in region:
-    #     x, y = cent.centroid
-    #     x, y = int(x), int(y)
-    #     image[x][y] = [1, 0.55, 0]
-    #     image[x+1][y] = image[x-1][y] = image[x][y+1] = image[x][y-1] = [1, 0.55, 0]
-    #     image[x+2][y] = image[x-2][y] = image[x][y+2] = image[x][y-2] = [1, 0.55, 0]
-
-    # image = ndi.binary_fill_holes(image)
-    # image = morphology.remove_small_objects(image)
-    # image = Image.fromarray((image * 255).astype(np.uint8))
-    # peri = cv2.arcLength(edges, True)
-    # approx = cv2.approxPolyDP(edges, 0.04 * peri, True)
-
-    # image = Image.fromarray((image * 255).astype(np.uint8))
-    # image.show()
-    # print(objects)
 
 
 if __name__ == "__main__":
